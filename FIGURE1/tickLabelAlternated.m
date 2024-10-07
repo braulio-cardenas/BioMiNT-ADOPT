653ef9d533473ee2ec4e94fa8f9b6b194e7fc7f4
braulio-cardenas <bcrdenas@uci.edu>
2024-10-07 16:47:25 -0700

    Update tickLabelAlternated.m

--- FIGURE1/tickLabelAlternated.m
+++ FIGURE1/tickLabelAlternated.m
@@ -3,8 +3,6 @@ function [strCell] = tickLabelAlternated(vect)
 %% Creates a list of string of values alternating between a tick value and a blank value ('')
 % Its purpose is to be used within a ticklabel function
 % 
-
-% strMatrix = arrayfun(@(x) num2str(x), vect, 'uniformOutput', 'false');
 strCell = arrayfun(@(x) num2str(x), vect, 'uniformOutput', 0);
 
 Nend = numel(strCell);
